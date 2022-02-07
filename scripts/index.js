0bd14da548027c54565d44b81414833b8aec645e
Maslenat <91131396+Maslenat@users.noreply.github.com>
2022-02-07 14:20:08 +1000

    redact

--- scripts/index.js
+++ scripts/index.js
@@ -8,40 +8,37 @@ const outputName = () => {
 
     massivstr = [...first];
 
-    let div =document.getElementById('outputname');
-    
-    if(first==0){
+    let div = document.getElementById('outputname');
 
-        div.innerHTML='';
+    if (first == 0) {
+
+        div.innerHTML = '';
         return;
 
-      }
+    }
 
-   if (massivstr.length!==3){
-    div.innerHTML='Введите три слова';
-       return;
-   }
-    
-    
-    else{ 
-        div.innerHTML='';
+    if (massivstr.length !== 3) {
+        div.innerHTML = 'Введите три слова';
+        return;
+    } else {
+        div.innerHTML = '';
 
-    let name = document.createElement("p");
-    name.innerHTML = `Фамилия: ${massivstr[0].charAt(0).toUpperCase() + massivstr[0].slice(1)}`;
-  
-    let secondname = document.createElement("p");
-    secondname.innerHTML = `Имя: ${massivstr[1].charAt(0).toUpperCase() + massivstr[1].slice(1)}`;
- 
-    let lastname = document.createElement("p");
-    lastname.innerHTML = `Отчество: ${massivstr[2].charAt(0).toUpperCase() + massivstr[2].slice(1)}`;
+        let name = document.createElement("p");
+        name.innerHTML = `Фамилия: ${massivstr[0].charAt(0).toUpperCase() + massivstr[0].slice(1)}`;
 
-    div.appendChild(secondname);
-    div.appendChild(name);
-    div.appendChild(lastname); }
+        let secondname = document.createElement("p");
+        secondname.innerHTML = `Имя: ${massivstr[1].charAt(0).toUpperCase() + massivstr[1].slice(1)}`;
+
+        let lastname = document.createElement("p");
+        lastname.innerHTML = `Отчество: ${massivstr[2].charAt(0).toUpperCase() + massivstr[2].slice(1)}`;
+
+        div.appendChild(name);
+        div.appendChild(secondname);
+        div.appendChild(lastname);
+    }
 
 
- 
+
 
 }
 document.getElementById('btn').onclick = outputName;
-
